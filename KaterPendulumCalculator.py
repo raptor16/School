58118d4253db032ac8a1e28bd1ca8a71879150fa
Florence Chan <florence@inside-65-104-123.wireless.utoronto.ca>
2015-10-14 15:56:40 -0400

    Physics Lab for 6 data points

--- KaterPendulumCalculator.py
+++ KaterPendulumCalculator.py
@@ -1,11 +1,18 @@
 import numpy as np
 
-Tprime = input ("What is your experimental T value?"")
-T = Tprime/8
+c=0
+Tprime = [0,0,0,0,0,0]
+T = [0,0,0,0,0,0]
+
+for i in range (0,6):
+    Tprime[i] = input ("What is your experimental T value?")
+    T[i] = Tprime[i]/8
+    c = c + T[i]
+
+T_avg =  c/6
 
 l = 1.02  # cm
 
-g = ((4* (np.pi)^2)*l)/T^2 # gravity
+g = (((4* (np.pi)**2)*l)/T_avg**2) # gravity
 
 print g
-
